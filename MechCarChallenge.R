c0d720f50f96589b2fa199c78802bf8dca355f71
jkehm <jkehm1837@gmail.com>
2023-04-12 13:03:46 -0400

    adding deliverable 3 items

--- MechCarChallenge.R
+++ MechCarChallenge.R
@@ -20,3 +20,23 @@ total_summary <- suspension_coil %>% summarise(Mean=mean(PSI), Median=median(PSI
 # Create lot_summary df 
 lot_summary<- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 # Once complete, push items to GitHub
+
+# Part 3: T-Tests on Suspension Coils
+
+# Use T-Test to determine if the PSI across all lots are statisticaly different from pop. mean
+t.test(suspension_coil$PSI, mu=1500)
+# Write 3 more RScripts using t.test() and its subset() argument to detrmine if the PSI for each manufacturing
+# lot is statiscally different from the pop. mean of 1,500 psi
+
+# Lot 1
+t.test((subset(suspension_coil, Manufacturing_Lot=='Lot1'))$PSI, mu=1500)
+# Lot 2
+t.test((subset(suspension_coil, Manufacturing_Lot=='Lot2'))$PSI, mu=1500)
+# Lot 3
+t.test((subset(suspension_coil, Manufacturing_Lot=='Lot3'))$PSI, mu=1500)
+# Once complete, push items to GitHub
+
+
+
+
+
